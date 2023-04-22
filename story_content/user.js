b3ab787777227c0c3c28043c3024ef73210805e1
unknown <ashurafa@hotmail.com>
2023-04-22 22:51:53 +0300

    1

--- story_content/user.js
+++ story_content/user.js
@@ -10,12 +10,9 @@ function ExecuteScript(strId)
 
 function Script1()
 {
-  // <script src="https://code.responsivevoice.org/responsivevoice.js?key=QWobFfIM"></script>
 
 
-// Var myText = GetPlayer().GetVar("myText");
-
-responsiveVoice.speak("Vibration Analysis Cat I Certification Practice Tests Prep Series (8 book series) Paperback Edition Looking to ace the Vibration Analyst Category I certification exam? Look no further than CAT I PREP I Package! With 8 comprehensive parts, each covering specific topics of the Body of Knowledge according to ISO 18436-2 standard, this package is the ultimate preparation tool. The questions are expertly arranged to provide the best possible learning experience and cover all topics in the ISO standard for Category I in the form of question banks. What's more, the package is not biased towards any specific certifying body, making it the perfect resource for all exam candidates. With over 1000 questions on offer, CAT I PREP I meets and exceeds standard requirements, ensuring you're fully equipped to tackle the exam. Plus, with an overall difficulty level slightly higher than that of the actual Cat I exams, you'll be more than ready to take on the challenge. Get CAT I PREP I Package today and get ready to succeed!" , "UK English Female");
+responsiveVoice.speak("Vibration Analysis Cat I Certification" , "US English Male");
 
 
 }
